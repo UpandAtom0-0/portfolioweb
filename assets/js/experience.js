7aca7070afebf5b29b8b5a7df5359780166a03e5
Vansh186 <78458980+Vansh186@users.noreply.github.com>
2021-11-29 11:10:56 +0530

    Update experience.js

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -6,11 +6,11 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
+    title: "Founder",
     cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
+    place: "VD Inc.",
     time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    desp: "A programming and Cyber Security Team that works 24/7 for betterment on various platforms.</li><li>Contact us at https://vdinc.ml .</li>",
   },
   {
     title: "Student Developer",
